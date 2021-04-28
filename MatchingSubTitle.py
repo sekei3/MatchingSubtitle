2d11267ba6c8678212d42e8252b1b27e6626cbdb
sekei3 <sekei@naver.com>
2021-04-28 19:30:04 +0900

    1. fix subtitle enumname 2. remove some debug code

--- MatchingSubTitle.py
+++ MatchingSubTitle.py
@@ -9,8 +9,8 @@ class VideoFileEnds(Enum):
     MP4 = '.mp4'
 
 class SubtitleFileEnds(Enum):
-    AVI = '.smi'
-    MKV = '.srt'
+    SMI = '.smi'
+    SRT = '.srt'
 
 def isVideoFile(filename):
     if any(filename.lower().endswith(vdoEnd.value)for vdoEnd in VideoFileEnds):
@@ -30,37 +30,36 @@ root = tkinter.Tk()
 root.withdraw()
 path_dir = filedialog.askdirectory(parent=root,initialdir="/",title='Please select a directory')
 
-path_dir = None
 
 if not path_dir:
-    print('Failed getting path')
+    print('failed getting path')
     exit()
-
 
 fileList = os.listdir( path_dir )
 
-videoFileList = [ file.capitalize() for file in fileList if isVideoFile(file) ]
-subFileList = [ file.capitalize() for file in fileList if isSubTitleFile(file) ]
+videoFileList = [ file for file in fileList if isVideoFile(file) ]
+subFileList = [ file for file in fileList if isSubTitleFile(file) ]
 
 if not videoFileList or not subFileList:
     print('empty file list')
+    exit()
 
+if len(videoFileList) != len(subFileList):
+    print('check not match count videofile and subtitle')
+    exit()
 
 videoFileList.sort()
 subFileList.sort()
 
+#print ("videoFileList: {}".format(videoFileList))
+#print ("subFileList: {}".format(subFileList))
 
-print ("videoFileList: {}".format(videoFileList))
-print ("subFileList: {}".format(subFileList))
-
-iCount = 0
-for file in subFileList:
+for i in range(len(subFileList)):
+    file = subFileList[i]
     srcExt = os.path.splitext(file)[1]
     src = os.path.join( path_dir, file )
-    dstfileName = os.path.splitext(videoFileList[iCount])[0]
+    dstfileName = os.path.splitext(videoFileList[i])[0]
     dst = os.path.join( path_dir, dstfileName + srcExt )
     os.rename(src, dst )
-    iCount += 1
 
-print ("videoFileList: {}".format(videoFileList))
-#print(os.getcwd())
+#print ("videoFileList: {}".format(videoFileList))
