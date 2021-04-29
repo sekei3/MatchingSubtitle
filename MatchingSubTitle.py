a7fe506ad4f8f033eaaab563540ad1c1f5611a6d
sekei3 <sekei@naver.com>
2021-04-30 00:24:05 +0900

    refactoring merge video, subtitle list

--- MatchingSubTitle.py
+++ MatchingSubTitle.py
@@ -13,16 +13,18 @@ class SubtitleFileEnds(Enum):
     SRT = '.srt'
 
 def isVideoFile(filename):
-    if any(filename.lower().endswith(vdoEnd.value)for vdoEnd in VideoFileEnds):
-        return True
-    else:
-        return False
+    for vdoEnd in VideoFileEnds:
+        if( filename.lower().endswith(vdoEnd.value) ):
+            return True
+
+    return False
 
 def isSubTitleFile(filename):
-    if any(filename.lower().endswith(subtitle.value)for subtitle in SubtitleFileEnds):
-        return True
-    else:
-        return False
+    for subtitle in SubtitleFileEnds:
+        if( filename.lower().endswith(sutitle.value)):
+            return True
+    
+    return False
 
 
 
@@ -54,11 +56,12 @@ subFileList.sort()
 #print ("videoFileList: {}".format(videoFileList))
 #print ("subFileList: {}".format(subFileList))
 
-for i in range(len(subFileList)):
-    file = subFileList[i]
-    srcExt = os.path.splitext(file)[1]
-    src = os.path.join( path_dir, file )
-    dstfileName = os.path.splitext(videoFileList[i])[0]
+merge_list = tuple(zip(videoFileList, subFileList)) 
+
+for vdo, sub in merge_list:
+    srcExt = os.path.splitext(sub)[1]
+    src = os.path.join( path_dir, sub )
+    dstfileName = os.path.splitext(vdo)[0]
     dst = os.path.join( path_dir, dstfileName + srcExt )
     os.rename(src, dst )
 
